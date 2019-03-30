96ad25b8b406279786a864d9eaab4329457caae4
teralion <bd@whitescape.com>
2019-03-30 15:05:50 +0300

    Add initial react files to index

---
+++ app/pages/Landing/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from './index.styl';
+
+function Landing() {
+  return (
+    <div className={css.landing}>
+      Watchers is under construction
+    </div>
+  );
+}
+
+export default Landing;
